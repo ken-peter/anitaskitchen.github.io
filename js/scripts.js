775bbc818c6b25d410f9d58ee0aef56d4975a6c9
ken-peter <Ken Peter>
2017-02-23 19:13:44 +0300

    added mouse-over

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
-
+// $(Document).ready(function(){
+//   $var happyHour=("#happy").click(function(){
+//     alert("Hello! Get a free meal for two for every Ksh 1000 spent from 10pm to 11pm over the weekdays and 7pm to 8pm over the weekends!!Amazing right!! ");
+//       });
+// });
+// $(document).ready(function(e){
+//   $("#happy").find("table").hover(function() {
+//       $("#basicModal").modal('show');
+//   }, function(){
+//       $("#basicModal").modal('hide');
+//       event.preventDefault();
+//   });
+// });
+>Rock on with the fanciest band in town eveery Friday night with a free bottle of champagne per couple. The first ten will get a shopping coupon. What are you waiting for!!
+Hello! Get a free meal for two for every Ksh 1000 spent from 10pm to 11pm over the weekdays and 7pm to 8pm over the weekends!!Amazing right!!
